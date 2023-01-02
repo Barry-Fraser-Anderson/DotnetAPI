78893379ffde9dcbec5e8bea6e3b6eb79f3d500b
Barry Anderson <barryanderson60@outlook.com>
2023-01-02 20:14:20 +0000

    EditUser and AddUSer

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -34,12 +34,59 @@ public class UserController : ControllerBase
   [HttpGet("GetSingleUser/{userId}")]
   public User GetSingleUser(int userId)
   {
-    string sql = @"
-      SELECT UserId, FirstName, LastName, Email, Gender, Active
-      FROM TutorialAppSchema.Users
-      WHERE Userid = " + userId.ToString();
+    string sql =
+      "SELECT UserId, FirstName, LastName, Email, Gender, Active" +
+      " FROM TutorialAppSchema.Users" +
+      " WHERE Userid = " + userId.ToString();
 
     User user = _dapper.LoadDataSingle<User>(sql);
     return user;
+  }
+
+  [HttpPut("EditUser")]
+  public IActionResult EditUser(User user)
+  {
+    string sql =
+       "UPDATE TutorialAppSchema.Users" +
+      $" SET FirstName = '{user.FirstName}'," +
+      $" LastName = '{user.LastName}'," +
+      $" Email = '{user.Email}'," +
+      $" Gender = '{user.Gender}'," +
+      $" Active = '{(user.Active ? 1 : 0)}' " +
+      $"WHERE UserId = {user.UserId}";
+
+    Console.WriteLine(sql);
+    if (_dapper.ExecuteSql(sql))
+    {
+      return Ok();
+    }
+
+    throw new Exception("Failed to update User");
+  }
+
+  [HttpPost("AddUser")]
+  public IActionResult AddUser(User user)
+  {
+    string sql =
+      "INSERT INTO TutorialAppSchema.Users(" +
+      "FirstName," +
+      "LastName," +
+      "Email," +
+      "Gender," +
+      "Active" +
+      ") VALUES (" +
+     $"'{user.FirstName}'," +
+     $"'{user.LastName}'," +
+     $"'{user.Email}'," +
+     $"'{user.Gender}'," +
+     $"'{(user.Active ? 1 : 0)}'" +
+      ")";
+
+    if (_dapper.ExecuteSql(sql))
+    {
+      return Ok();
+    }
+
+    throw new Exception("Failed to add User");
   }
 }
